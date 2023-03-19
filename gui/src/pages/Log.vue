52634f2624edee4c27d1fdec0169bd2d44deea64
shellrow <81893184+shellrow@users.noreply.github.com>
2023-03-19 12:24:27 +0900

    Update Log.vue

--- gui/src/pages/Log.vue
+++ gui/src/pages/Log.vue
@@ -5,6 +5,64 @@ import { debounce } from 'lodash';
 
 const searching = ref(false);
 
+const probeTypes = [
+  {
+    value: 'port_scan',
+    label: 'Port Scan',
+  },
+  {
+    value: 'host_scan',
+    label: 'Host Scan',
+  },
+  {
+    value: 'ping',
+    label: 'Ping',
+  },
+  {
+    value: 'traceroute',
+    label: 'Traceroute',
+  },
+];
+
+const optionDateRange = ref('');
+
+const shortcuts = [
+  {
+    text: 'Last week',
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 7)
+      return [start, end]
+    },
+  },
+  {
+    text: 'Last month',
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 30)
+      return [start, end]
+    },
+  },
+  {
+    text: 'Last 3 months',
+    value: () => {
+      const end = new Date()
+      const start = new Date()
+      start.setTime(start.getTime() - 3600 * 1000 * 24 * 90)
+      return [start, end]
+    },
+  },
+];
+
+const searchOption = reactive({
+    target_host: "",
+    probe_types: [],
+    start_date: "",
+    end_date: ""
+});
+
 const searchResult = ref([]);
 
 const searchLog = async () => {
@@ -17,6 +75,10 @@ const searchLog = async () => {
 
 const clickSearch = (event) => {
     searchLog();
+}
+
+const handleOpen = (index, row) => {
+  console.log(index, row);
 }
 
 onMounted(() => {
@@ -40,6 +102,32 @@ onUnmounted(() => {
 .item {
   margin-bottom: 18px;
 }
+
+.opt-date-picker {
+  display: flex;
+  width: 100%;
+  padding: 0;
+  flex-wrap: wrap;
+}
+
+.opt-date-picker .block {
+  padding: 30px 0;
+  text-align: center;
+  border-right: solid 1px var(--el-border-color);
+  flex: 1;
+}
+
+.opt-date-picker .block:last-child {
+  border-right: none;
+}
+
+.opt-date-picker .opt-date-picker-title {
+  display: block;
+  color: var(--el-text-color-secondary);
+  font-size: 14px;
+  margin-bottom: 20px;
+}
+
 </style>
 
 <template>
@@ -52,6 +140,44 @@ onUnmounted(() => {
             </div>
         </template>
         <!-- Header -->
+        <!-- Option -->
+        <el-row :gutter="20">
+            <el-col :span="6">
+                <p style="font-size: var(--el-font-size-small)">Probe Type</p>
+                <el-select
+                v-model="searchOption.probe_types"
+                multiple 
+                collapse-tags 
+                placeholder="Select"
+                style="width: 240px"
+                >
+                <el-option
+                    v-for="item in probeTypes"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                />
+                </el-select>
+            </el-col>
+            <el-col :span="6">
+                <p style="font-size: var(--el-font-size-small)">Target</p>
+                <el-input v-model="searchOption.target_host" placeholder="Address or Name" />
+            </el-col>
+            <el-col :span="6">
+                <p style="font-size: var(--el-font-size-small)">Probe date range</p>
+                <el-date-picker
+                    v-model="optionDateRange"
+                    type="daterange"
+                    unlink-panels
+                    range-separator="To"
+                    start-placeholder="Start date"
+                    end-placeholder="End date"
+                    :shortcuts="shortcuts"
+                    size="default"
+                />
+            </el-col>
+        </el-row>
+        <!-- Option -->
     </el-card>
     <div v-loading="searching" element-loading-text="Searching..." class="mt-2">
         <div v-if="searchResult.length > 0">
@@ -63,19 +189,21 @@ onUnmounted(() => {
             >
             </el-descriptions>
             <el-table :data="searchResult" style="width: 100%" class="mt-2">
-                <el-table-column prop="id" label="ID">
+                <el-table-column prop="id" label="ID" width="80">
                     <template #default="scope">
                         <el-popover effect="light" trigger="hover" placement="top" width="auto">
                             <template #default>
                                 <div>{{ scope.row.probe_id }}</div>
                             </template>
                             <template #reference>
-                                <el-tag>{{ scope.row.id }}</el-tag>
+                                <el-button size="small" @click="handleOpen(scope.$index, scope.row)">
+                                    {{ scope.row.id }}
+                                </el-button>
                             </template>
                         </el-popover>
                     </template>
                 </el-table-column>
-                <el-table-column prop="probe_type" label="Probe Type"  />
+                <el-table-column prop="probe_type_id" label="Probe Type"  />
                 <el-table-column prop="probe_target_addr" label="Target Addr" />
                 <el-table-column prop="probe_target_name" label="Target Name" />
                 <el-table-column prop="protocol_id" label="Protocol" />
