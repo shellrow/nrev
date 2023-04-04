f064fe0b5c9852e028f6937aaaf543905844bdb6
shellrow <81893184+shellrow@users.noreply.github.com>
2023-04-05 00:13:12 +0900

    Update shared.rs

--- core/src/process/shared.rs
+++ core/src/process/shared.rs
@@ -1,10 +1,8 @@
 use std::env;
-//use runas;
 
 pub fn restart_as_root() {
-    println!("check current exe and args...");
-    println!("{}", env::current_exe().unwrap().display());
-    println!("{}", env::args().collect::<Vec<String>>().join(" "));
+    //println!("{}", env::current_exe().unwrap().display());
+    //println!("{}", env::args().collect::<Vec<String>>().join(" "));
     let args = env::args().collect::<Vec<String>>();
     let mut cmd = runas::Command::new(&env::current_exe().unwrap());
     cmd.gui(true);    
@@ -12,12 +10,4 @@ pub fn restart_as_root() {
     cmd.args(&args[1..]);
     println!("{}",cmd.status().expect("failed to execute"));
     std::process::exit(0);
-    /* println!(
-        "Status: {}",
-        runas::Command::new("id")
-            .gui(true)
-            .force_prompt(true)
-            .status()
-            .expect("failed to execute")
-    ); */
 }
