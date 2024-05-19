6a10d31601d4bb8a7b1e9a9bcff98847742c34ee
shellrow <81893184+shellrow@users.noreply.github.com>
2024-05-19 20:56:18 +0900

    Windows dependencies support

--- nerum/src/handler/check.rs
+++ nerum/src/handler/check.rs
@@ -1,6 +1,6 @@
 use clap::ArgMatches;
-use nerum_core::sys::dep;
+use nerum_core::dep;
 
 pub fn check_dependencies(_arg: &ArgMatches) {
-    dep::resolve_dependencies();
+    let _ = dep::check_dependencies();
 }
